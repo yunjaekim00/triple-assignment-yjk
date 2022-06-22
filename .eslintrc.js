314ba26a38927caf043e6eb2a9b8a52164697951
yunjaekim <yunjaekim007@gmail.com>
2022-06-22 22:56:11 +0900

    deploy

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
-// const createConfig = require('@titicaca/eslint-config-triple/create-config')
+// const createConfig = require("@titicaca/eslint-config-triple/create-config");
 
-// module.exports = createConfig({
-//   type: 'frontend'
-// })
+// module.exports = createConfig({ type: "frontend" });
 
 module.exports = {
   extends: [
-    "@titicaca/eslint-config-triple/",
+    "@titicaca/eslint-config-triple",
     "@titicaca/eslint-config-triple/frontend",
   ],
 };
